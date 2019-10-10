380d815cf1a96838ca7c512bc95221a4b77c841e
fisherprime <mbock.ernie40@gmail.com>
2019-10-10 17:21:29 +0300

    feat(*): Add git2 crate & initial code
    Provide resources that will allow for the clone & update of a git
    gitignore template repository.

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+extern crate git2;
+
 // TODO: populate this
 #[allow(dead_code)]
 pub fn parse_languages_tools() {}
@@ -13,3 +15,32 @@ pub fn generate_gitignore() {
 // TODO: populate this
 #[allow(dead_code)]
 pub fn update_gitignore_repo() {}
+
+// TODO: populate this
+fn update_gitignore_repo(repo_dir: &str, repo_url: &str) -> Result<(), git2::Error> {
+    match Repository::open(repo_dir) {
+        Ok(_repo) => {
+            /* repo.stash_save(signature, flags)?;
+             * repo.stash_drop(index)?; */
+
+            Ok(())
+        }
+        Err(_) => {
+            info!("Repository not cached locally, cloning");
+
+            match Repository::clone_recurse(repo_url, repo_dir) {
+                Ok(_) => {
+                    debug!("Repository cloned from upstream");
+
+                    Ok(())
+                }
+                Err(err) => {
+                    warn!("Failed to clone: {} into: {}", repo_url, repo_dir);
+
+                    Err(err)
+                }
+            }
+        }
+    }
+}
+
